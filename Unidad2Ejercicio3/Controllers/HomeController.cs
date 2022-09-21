596c6e93209843d86a4780f09383299d749ddf8f
alumnocca4 <alumnocca4@CCA4-14-9>
2022-09-21 16:49:12 -0500

    Terminacion del proyecto

--- Unidad2Ejercicio3/Controllers/HomeController.cs
+++ Unidad2Ejercicio3/Controllers/HomeController.cs
@@ -17,14 +17,24 @@ namespace Unidad2Ejercicio3.Controllers
             return View(presientes);
         }
 
-        public IActionResult Biografia(int id)
+        public IActionResult Biografia(string id)
         {
+            //Si esta nulo el string que nos lleve al index
+            if (string.IsNullOrWhiteSpace(id))
+                return RedirectToAction("Index");
 
-
+            //Debemos remplazar los guines del url a espacios en blanco para poder buscar 
+            //Al objeto en la base de datos
+            id = id.Replace("-", " ");
 
             //Carga explicita
-            var presidente = context.Presidentes.Include(x => x.IdPartidoPoliticoNavigation).Include(x => x.IdEstadoRepublicaNavigation).FirstOrDefault(x => x.Id == id);
+            var presidente = 
+                context.Presidentes.Include
+                (x => x.IdPartidoPoliticoNavigation).
+                Include(x => x.IdEstadoRepublicaNavigation)
+                .FirstOrDefault(x => x.Nombre == id);
 
+            //Si es presidente no es null que nos lleve a la pagina biografia
             if (presidente != null)
                 return View(presidente);
             else
