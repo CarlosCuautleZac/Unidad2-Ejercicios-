9be1aa03c20cb8d8bf9df6dee6d6cde4abfbb698
alumnocca5 <alumnocca5@CCCA5-014>
2022-09-22 16:52:33 -0500

    Creacion de nuevo proyecto. Configuracion de startup y las vistas

---
+++ Ejercicio4/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Datos()
+        {
+            return View();
+        }
+
+        public IActionResult Reticula()
+        {
+            return View();
+        }
+    }
+}
