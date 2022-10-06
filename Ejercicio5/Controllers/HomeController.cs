dd6a579d748cfc1a0731e257d652ac0f70bac512
Operador <Operador@CCA5-23>
2022-10-06 16:55:31 -0500

    cambios

--- Ejercicio5/Controllers/HomeController.cs
+++ Ejercicio5/Controllers/HomeController.cs
@@ -1,29 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ejercicio5.Models;
+using Ejercicio5.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ejercicio5.Controllers
 {
     public class HomeController : Controller
     {
+        pixarContext context = new pixarContext();
+
+        [Route("/")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("Peliculas")]
         public IActionResult Peliculas()
         {
-            return View();
+            var peliculas = context.Pelicula.Select(x=> new PeliculasViewModel() { Id = x.Id, Nombre=x.Nombre}).OrderBy(x => x.Nombre);
+
+            return View(peliculas);
         }
 
+        [Route("Cortos")]
         public IActionResult Cortos()
         {
             return View();
         }
-
-        public IActionResult Pelicula()
+        [Route("/Pelicula/{id}")]
+        public IActionResult Pelicula(string id)
         {
+            var pelicula = context.Pelicula.FirstOrDefault(x => x.Nombre==id.Replace("-"," "));
+
+            if (pelicula == null)
+            {
+                RedirectToAction("Peliculas");
+            }
+
             return View();
         }
-
+        [Route("Corto/{id}")]
         public IActionResult Corto()
         {
             return View();
---
+++ Ejercicio5/Models/ViewModels/PeliculasViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejercicio5.Models.ViewModels
+{
+    public class PeliculasViewModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = "";
+    }
+}
