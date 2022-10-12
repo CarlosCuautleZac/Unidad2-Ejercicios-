97333c98d4c9c85267d18f19f4d602de84ac21ef
Operador <Operador@CCA5-23>
2022-10-11 19:41:40 -0500

    cambios en viewmodel

--- Ejercicio6/Controllers/HomeController.cs
+++ Ejercicio6/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ejercicio6.Models;
+using Ejercicio6.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ejercicio6.Controllers
 {
     public class HomeController : Controller
     {
+        villancicosContext context = new();
+
         public IActionResult Index()
         {
-            return View();
+            var villancicos = context.Villancicos.Select(x => new IndexViewModel
+            {
+                Nombre = x.Nombre,
+                Informacion = x.Información
+            }).OrderBy(x=>x.Nombre);
+
+            
+
+            return View(villancicos);
         }
 
         public IActionResult Villancico()
---
+++ Ejercicio6/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejercicio6.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public string? Nombre { get; set; }
+        public string? Informacion { get; set; }
+    }
+}
